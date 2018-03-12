128a8f08ee17b37f3965b2f81927d6d7ec4b2644
Richard <richard@tlgd.co.uk>
2018-03-12 15:02:57 +0000

    In the SearchBar class, added an event handler method for onInputChange, add this as the value of a new onChange property to the input element. Pass the vent as an argument, and console log out the value each time event is fired. Also added more comments

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,9 +1,20 @@
 import React, { Component } from 'react';
 
+// Class based component extends React.Component
 class SearchBar extends Component{
+    // Render method required on all class based components
     render(){
-        return <input />;
+        // All input elements emit a change event (vanilla html)
+        // This line create a new input element and gives it a onChange 'prop'
+        return <input onChange={this.onInputChange} />;  // Returns JSX. React specific property, onChange i.e. (on, TheEventName)
+    }
+
+    // Event handler. Naming convention on/handleElementEvent I.e. onInputChange
+    // Create the event handler and pass it to the input we want to watch the event on i.e. input onChange={this.onInputChange}
+    onInputChange(event){
+        console.log(event.target.value);
     }
 }
 
+// Export only a selected part of the coded
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom'
-import SearchBar from './components/search_bar';
+import ReactDOM from 'react-dom' // Namespaced by npm so no directory needed
+import SearchBar from './components/search_bar'; // Not namespaced so need relative directory from this file
 
-const API_KEY = 'AIzaSyDU0S7COi9iQ_sbDp_tKHTLtwog64i9k30';
+const API_KEY = 'AIzaSyDU0S7COi9iQ_sbDp_tKHTLtwog64i9k30'; // YouTube API key
 
 // Create a new Component
 // This component should produce some html
