5e10209e72d16d97513dd464158f822aea2f6182
Richard <richard@tlgd.co.uk>
2018-03-13 14:19:20 +0000

    Import youtube search package. Call it will the api key and and search term and console log out the returned data. Which is five objects with info about a video

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom' // Namespaced by npm so no directory needed
+import React from 'react'; // Namespaced by npm so no directory needed
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; // Not namespaced so need relative directory from this file
 
 const API_KEY = 'AIzaSyDU0S7COi9iQ_sbDp_tKHTLtwog64i9k30'; // YouTube API key
+
+YTSearch({key: API_KEY, term: 'surf wipeouts'}, function(data){
+    console.log(data);
+});
 
 // Create a new Component
 // This component should produce some html
