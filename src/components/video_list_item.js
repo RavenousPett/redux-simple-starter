23fcc122040e70e6c14261c551ff937d34228788
Richard <richard@tlgd.co.uk>
2018-03-17 14:37:08 +0000

    Define video inside of vide list item.

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -1,6 +1,8 @@
 import React from 'react';
 
-const VideoListItem = (props) => {
+// const VideoListItem = ({video}) is the same as doing const video = props.video;
+const VideoListItem = ({video}) => {
+
     return <li>Video</li>;
 };
 
