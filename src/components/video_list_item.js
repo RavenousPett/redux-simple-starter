81614fd906afd1b3cee88ed4dba4ad94c6d89504
Richard <richard@tlgd.co.uk>
2018-03-17 14:44:16 +0000

    Adding JSX markup to vide list item

--- src/components/video_list_item.js
+++ src/components/video_list_item.js
@@ -3,7 +3,21 @@ import React from 'react';
 // const VideoListItem = ({video}) is the same as doing const video = props.video;
 const VideoListItem = ({video}) => {
 
-    return <li>Video</li>;
+    return (
+        <li className="list-group-item">
+            <div className="video-list media">
+
+                <div className="media-left">
+                    <img className="media-object" />
+                </div>
+
+                <div className="media-body">
+                    <div className="media-heading"></div>
+                </div>
+
+            </div>
+        </li>
+    );
 };
 
 export default VideoListItem;
